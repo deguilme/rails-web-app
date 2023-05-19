9325fda8c6d080282246b80eeee8592c62de2962
Elliott <deguilme@uwm.edu>
2023-05-19 12:54:04 -0500

    initial commit

---
+++ widgets/ISLayers/nls/strings.js
@@ -0,0 +1,18 @@
+define({
+    root:({
+        layerSaved: "Layer is already saved.",
+        restricted: "Restricted.Please rename.",
+        entername : "Enter the name:",
+        overwrite : "Name already exists in the Layer list. Do you want to overwrite it?",
+		resultsave: "Save the Result layer in the layer list",
+		result: "Result",
+        resultLayer : "Add the Result layer in the secondary layer list",
+		primary: "Primary",
+		secondary: "Secondary",
+        copy : "Copy primary to secondary",
+        swap :"Swap primary and secondary",
+		selectPrimaryLayer: "Select primary layer",
+		selectSecondaryLayer: "Select secondary layer",
+		noImageryLayer: "No Imagery layer"
+    })  
+});
---
+++ widgets/ISLayers/setting/nls/strings.js
@@ -0,0 +1,12 @@
+define({
+  root: ({
+	 primaryLayer: "Primary Layer",
+     secondaryLayer: "Secondary Layer",
+     selectLayer: "Select layer",	 
+     arrangement: "Arrangement",
+     autoUpdate: "Auto Update",
+     respectCurrentMapScale: "Respect Current Map Scale"
+  }),
+  "ar": 1,
+  "fr": 1
+});
